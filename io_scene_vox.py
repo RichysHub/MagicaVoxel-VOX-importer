1bfb7f8ab730676438b70c85c63c7e16af653aeb
Richard Spencer <richardjspencer93@gmail.com>
2017-10-17 22:36:01 +0100

    Started to add material support from vox format

--- io_scene_vox.py
+++ io_scene_vox.py
@@ -12,20 +12,20 @@ import os
 import bpy
 from bpy.props import StringProperty, IntProperty, FloatProperty, BoolProperty, CollectionProperty
 from bpy_extras.io_utils import ImportHelper
+from collections import namedtuple
 
 import struct
 
 bl_info = {
     "name": "MagicaVoxel VOX format",
     "author": "Richard Spencer",
-    "blender": (2, 74, 0),
     "location": "File > Import-Export",
     "description": "Import MagicaVoxel .vox files",
-    "warning": "",
-    "wiki_url": "",
     "support": 'TESTING',
     "category": "Import-Export"}
 
+material_spec = namedtuple('material_spec', ['type_', 'weight', 'Plastic', 'Roughness', 'Specular',
+                                             'IOR', 'Attenuation', 'Power', 'Glow', 'isTotalPower'])
 
 class ImportVOX(bpy.types.Operator, ImportHelper):
     """Load a MagicaVoxel VOX File"""
@@ -87,7 +87,6 @@ class ImportVOX(bpy.types.Operator, ImportHelper):
         layout.prop(self, "use_shadeless")
 
 
-
 def import_vox(path, *, voxel_spacing=1, voxel_size=1,
                use_bounds=False, start_voxel=None, end_voxel=None,
                use_palette=True, gamma_correct=True, gamma_value=2.2, use_shadeless=False):
@@ -98,6 +97,7 @@ def import_vox(path, *, voxel_spacing=1, voxel_size=1,
 
         voxels = []
         palette = {}
+        material_specs = {}
 
         # assert is VOX 150 file
         assert (struct.unpack('<4ci', vox.read(8)) == (b'V', b'O', b'X', b' ', 150))
@@ -112,7 +112,8 @@ def import_vox(path, *, voxel_spacing=1, voxel_size=1,
         while True:
             try:
                 *name, s_self, s_child = struct.unpack('<4cii', vox.read(12))
-                assert (s_child == 0)  # sanity check
+                # TODO: disabled while aligning chunks for materials
+                # assert (s_child == 0)  # sanity check
                 name = b''.join(name).decode('utf-8')  # unsure of encoding..
             except struct.error:
                 # end of file
@@ -122,8 +123,7 @@ def import_vox(path, *, voxel_spacing=1, voxel_size=1,
                 num_models = struct.unpack('<i', vox.read(4))
             elif name == 'SIZE':
                 # model size
-                # x, y, z = struct.unpack('<3i', vox.read(12))
-                vox.read(12)
+                x, y, z = struct.unpack('<3i', vox.read(12))
             elif name == 'XYZI':
                 # voxel data
                 num_voxels, = struct.unpack('<i', vox.read(4))
@@ -136,11 +136,20 @@ def import_vox(path, *, voxel_spacing=1, voxel_size=1,
                     palette.update({col+1: struct.unpack('<4B', vox.read(4))})
             elif name == 'MATT':
                 # material
-                matt_id, mat_type, weight = struct.unpack('<iif', vox.read(12))
+                mat_id, mat_type, weight = struct.unpack('<iif', vox.read(12))
+                byte_value, = struct.unpack('<i', vox.read(4))
+                reversed_bitstring = '{:032b}'.format(byte_value)[::-1]
+                properties = ['Plastic', 'Roughness', 'Specular', 'IOR', 'Attenuation', 'Power', 'Glow', 'isTotalPower']
+                property_bits = {}
+                property_values = {}
+                for pos, property_ in enumerate(properties):
+                    bit_value = reversed_bitstring[pos]
+                    property_bits.update({property_: bit_value})
+                    if bit_value == '1' and property_ != 'isTotalPower':  # isTotalPower doesn't get a value
+                        property_values.update({property_: struct.unpack('<f', vox.read(4))})
+                print(property_bits)
+                material_specs.update({mat_id: material_spec(type_=mat_type, weight=weight, **property_values)}) # isTotalPower is never supplied by this
 
-                prop_bits, = struct.unpack('<i', vox.read(4))
-                binary = bin(prop_bits)
-                # Need to read property values, but this gets fiddly
                 # TODO: finish implementation
             else:
                 # Any other chunk, we don't know how to handle
@@ -153,24 +162,49 @@ def import_vox(path, *, voxel_spacing=1, voxel_size=1,
         end = min([end_voxel, len(voxels)])
         voxels = voxels[start_voxel:end]
 
+    use_materials = True  # TODO: Move to an option on import
+
     if use_palette:
         used_palette_indices = set()
         for voxel in voxels:
-            # This is done here, so to avoid adding materials for voxels not in bounds
-            used_palette_indices.add(voxel[3])  # record the pallette entry is used
+            # avoid adding materials for voxels not in bounds
+            used_palette_indices.add(voxel[3])  # record pallette entry as used
 
         if not gamma_correct:
-            gamma_value = 1
+            gamma_value = 1  # Value of 1 leaves color unaffected
         mat_palette = {}
 
         for index in used_palette_indices:
             palette_entry = palette[index]
             material = bpy.data.materials.new("Voxel_mat{}".format(index))
             material.diffuse_color = [pow(col/255, gamma_value) for col in palette_entry[:3]]
-            material.diffuse_intensity = 1.0
+            # TODO: add specular, if materials being used
+            # TODO: refactor so that we work on assumption of diffuse, but stay DRY
+            if use_materials:
+                spec = material_specs[index]
+                material.diffuse_intensity = spec.Plastic  # diffuse intensity is value of plastic?
+                material.specular_intensity = spec.Roughness  # TODO: yeah? scale needs inverting & scaling
+                mat_type = spec.type_
+                if mat_type== 1:
+                    # metal
+                    material.raytrace_mirror.use = True
+                    material.raytrace_mirror.reflect_factor = spec.weight
+                elif mat_type ==2:
+                    # glass
+                    material.use_transparency = True
+                    material.transparency_method = 'Raytrace'
+                    material.raytrace_transparency.ior = (spec.IOR*3.75)+0.25  # Map to 0.25 -> 4 range
+                elif mat_type ==3:
+                    # emissive
+                    material.emit = spec.Power*2  # TODO: maybe? how do we do the glow even?
+                    # Glow effect is currently planned to be done via a composite note setup, cycles only :/
+            else:
+                material.diffuse_intensity = 1.0
+
             material.alpha = palette_entry[3]
             material.use_shadeless = use_shadeless
             mat_palette.update({index: material})
+
 
     # peel first voxel information
     voxel, *voxels = voxels
