290a2ed7d0f9c77171804097d60d7a111d09b3f8
wizardgsz <wizardgsz@users.noreply.github.com>
2019-09-29 01:31:51 +0200

    Update io_scene_vox.py
    It works:
    
    Reading at dec: 20 hex: 14
    Parsing chunk 'PACK':
    Reading at dec: 36 hex: 24
    Parsing chunk 'SIZE':
    Reading at dec: 60 hex: 3C
    Parsing chunk 'XYZI':
    Reading at dec: 12748 hex: 31CC
    Parsing chunk 'RGBA':
    Reading at dec: 13784 hex: 35D8
    Parsing chunk 'MATT':
    Reading at dec: 13832 hex: 3608
    Parsing chunk 'MATT':
    Reading at dec: 13872 hex: 3630
    Parsing chunk 'MATT':
    Reading at dec: 13916 hex: 365C
    Parsing chunk 'MATT':
    Reading at dec: 13964 hex: 368C
    
    Co-Authored-By: RichysHub <richy_s@hotmail.co.uk>

--- io_scene_vox.py
+++ io_scene_vox.py
@@ -195,7 +195,8 @@ def import_vox(path, *, voxel_spacing=1, voxel_size=1,
                 binary = bin(prop_bits)
                 # Need to read property values, but this gets fiddly
                 # TODO: finish implementation
-                vox.seek(_offset + 12 + s_self, 0)
+                # We have read 16 bytes of this chunk so far, ignoring remainder
+                vox.read(s_self - 16)
             else:
                 # Any other chunk, we don't know how to handle
                 # This puts us out-of-step
