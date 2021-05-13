0cf437e6aabd74ce0d17e430b76b4db966be7a84
Dagfinn Parnas <dagfinn.parnas@gmail.com>
2021-05-13 21:52:56 +0200

    set version to 0.5.0

--- io_scene_meebits_vox.py
+++ io_scene_meebits_vox.py
@@ -23,7 +23,7 @@ import struct
 bl_info = {
     "name": "Meebit (.vox)",
     "author": "Dagfinn Parnas based on forked code from Richard Spencer, Gabriele Scibilia",
-    "version": (2, 2, 1),
+    "version": (0, 5, 0),
     "blender": (2, 80, 0),
     "location": "File > Import-Export",
     "description": "Import Meebit from .vox file",
